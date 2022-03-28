b6ba89680692e0adc8c4e8777d2100a9af7957f0
hellena <tessehellen@gmail.com>
2022-03-29 00:47:53 +0300

    created results.js and meaning.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,34 @@
 import React, {useState} from "react";
+import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    const [keyWord, setKeyword] = useState("");
+    const [keyword, setKeyword] = useState("");
+    const [results, setResults] = useState({});
+
+    function handleResponse(response) {
+        setResults(response.data[0]);
+    }
+
     function search(event) {
         event.preventDefault();
 
-        function handleKeywordChange(event) {
-            setKeyword(event.terget.value);
-        }
-}
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+       axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.terget.value);
+    }
+    
 
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results />
         </div>
     );
- }
+}
